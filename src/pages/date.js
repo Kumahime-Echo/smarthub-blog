068c0d995981623a4a750b8bdcf81de6c8ab5553
zhouwenhao <zhouwenhao@qince.com>
2024-05-29 16:37:36 +0800

    feat: blog

--- src/pages/date.js
+++ src/pages/date.js
@@ -1,10 +1,3 @@
-import { parseISO, format } from "date-fns";
-
 export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return (
-    <time dateTime={dateString} className="text-gray-500">
-      {format(date, "yyyy年MM月dd日")}
-    </time>
-  );
+  return <time className="text-gray-500">{dateString}</time>;
 }
